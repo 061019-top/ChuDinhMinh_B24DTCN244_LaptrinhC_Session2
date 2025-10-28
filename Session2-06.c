ae2985fb48940d1e6a70e98afc3a80112f3bff81
Dean <chudinhminh21@gmail.com>
2025-10-28 16:03:37 +0700

    Delete Session2-06.c

--- Session2-06.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    float PI = 3.14;
-    float banKinh = 6.0;
-    float chuVi = 2 * PI * banKinh;
-    float dienTich = PI * banKinh * banKinh;
-    return 0;
-}
-
