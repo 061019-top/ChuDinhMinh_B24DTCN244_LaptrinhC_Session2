cd4ea1c865287651d531c4e8fd845686d38ce0a4
Dean <chudinhminh21@gmail.com>
2025-10-28 16:03:24 +0700

    Delete Session2-05.c

--- Session2-05.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int chieuDai = 15;
-    int chieuRong = 6;
-    int chuVi = 2 * (chieuDai + chieuRong);
-    int dienTich = chieuDai * chieuRong;
-    return 0;
-}
-
